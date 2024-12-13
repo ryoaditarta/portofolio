d2c56b9ad677996be565e44fab73b526055084d5
ryoaditarta <ryoaditarta@gmail.com>
2024-12-13 23:47:46 +0700

    fix navigation problems

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,28 +15,28 @@ const Navbar = () => {
       <ul className="flex flex-row gap-5 justify-center">
         <Link href="/">
           <li
-            className={`bg-slate-700 p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/') ? 'bg-slate-500' : 'hover:bg-slate-500'}`}
+            className={` p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/') ? 'bg-slate-500' : 'bg-slate-700'} hover:bg-slate-500`}
           >
             <FaHome className="text-white w-10 h-10" />
           </li>
         </Link>
         <Link href="/activity">
           <li
-            className={`bg-slate-700 p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/activity') ? 'bg-slate-500' : 'hover:bg-slate-500'}`}
+            className={` p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/activity') ? 'bg-slate-500' : 'bg-slate-700'} hover:bg-slate-500`}
           >
             <FaTools className="text-white w-10 h-10" />
           </li>
         </Link>
         <Link href="/contact">
           <li
-            className={`bg-slate-700 p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/contact') ? 'bg-slate-500' : 'hover:bg-slate-500'}`}
+            className={` p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/contact') ? 'bg-slate-500' : 'bg-slate-700'} hover:bg-slate-500`}
           >
             <FaPhone className="text-white w-10 h-10" />
           </li>
         </Link>
         <Link href="/games">
           <li
-            className={`bg-slate-700 p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/games') ? 'bg-slate-500' : 'hover:bg-slate-500'}`}
+            className={` p-4 inline-flex justify-center cursor-pointer rounded-lg ${isActive('/games') ? 'bg-slate-500' : 'bg-slate-700'} hover:bg-slate-500`}
           >
             <FaGamepad className="text-white w-10 h-10" />
           </li>
