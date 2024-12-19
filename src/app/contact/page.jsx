230770e51a7190aa19c556d25faffff1511c6d36
ryoaditarta <ryoaditarta@gmail.com>
2024-12-19 19:31:05 +0700

    cv update

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { Lato } from "next/font/google";
-import { FaLinkedin, FaGithub, FaInstagram, FaCode } from 'react-icons/fa'; 
-import Link from 'next/link'; 
+import { FaLinkedin, FaGithub, FaInstagram, FaCode } from 'react-icons/fa';
+import Link from 'next/link';
 
 const lato = Lato({
   subsets: ['latin'],
@@ -33,7 +33,7 @@ const Page = () => {
         <h1 className={`${lato.className} sm:text-4xl text-xl font-bold text-white text-center sm:my-10 mt-5 mb-3`}>Curriculum Vitae:</h1>
         <div className="flex justify-center">
           <a
-            href="/cvryo.pdf" 
+            href="/cvryo.pdf"
             target="_blank"
             className="px-6 py-3 bg-slate-700 text-white font-bold rounded hover:bg-blue-500"
           >
@@ -48,13 +48,13 @@ const Page = () => {
           <img src="https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white" alt="Python" />
           <img src="https://img.shields.io/badge/JavaScript-007ACC?style=for-the-badge&logo=javascript&logoColor=white" alt="JavaScript" />
           <img src="https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white" alt="PHP" />
-          <img src="https://img.shields.io/badge/Shell_Script-121011?style=for-the-badge&logo=gnu-bash&logoColor=white" alt="bash"/>
-          <img src="https://img.shields.io/badge/React-61DAFB?logo=react&logoColor=000&style=for-the-badge" alt="React Badge"/>
-          <img src="https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="react_native"/>
-          <img src="https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white" alt="laravel"/>
-          <img src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="tailwind"/>
-          <img src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D" alt="vue"/>
-          <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="bootstrap"/>
+          <img src="https://img.shields.io/badge/Shell_Script-121011?style=for-the-badge&logo=gnu-bash&logoColor=white" alt="bash" />
+          <img src="https://img.shields.io/badge/React-61DAFB?logo=react&logoColor=000&style=for-the-badge" alt="React Badge" />
+          <img src="https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="react_native" />
+          <img src="https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white" alt="laravel" />
+          <img src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="tailwind" />
+          <img src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D" alt="vue" />
+          <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="bootstrap" />
         </div>
       </div>
     </div>
